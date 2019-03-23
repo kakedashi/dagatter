f15772efce7e15b2c455acbabf2d0fe182421d5b
Ryota Misumi <3delta7@gmail.com>
2019-03-23 18:37:35 +0900

    flywayの設定を追加

--- migrate/V1__create_tables.sql
+++ migrate/V1__create_tables.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS users
-  (id SERIAL PRIMARY KEY,
+  (id TEXT NOT NULL PRIMARY KEY,
     name TEXT NOT NULL,
     image_url TEXT);
 
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS tweets(
     tweet TEXT NOT NULL,
     like_count INTEGER NOT NULL,
     retweet_count INTEGER NOT NULL,
-    user_id INTEGER REFERENCES users(id));
+    user_id TEXT REFERENCES users(id));
 
 CREATE TABLE IF NOT EXISTS dagashis(
     id SERIAL PRIMARY KEY,
@@ -16,12 +16,15 @@ CREATE TABLE IF NOT EXISTS dagashis(
     price INTEGER NOT NULL);
 
 CREATE TABLE IF NOT EXISTS dagashi_association(
-    user_id INTEGER REFERENCES users(id),
+    user_id TEXT REFERENCES users(id),
     tweet_id INTEGER REFERENCES tweets(id),
     dagashi_id INTEGER REFERENCES dagashis(id));
 
 -- for ROLLBACK
+-- DROP SEQUENCE IF EXISTS users_id_seq;
 -- DROP TABLE IF EXISTS users;
+-- DROP SEQUENCE IF EXISTS tweets_id_seq;
 -- DROP TABLE IF EXISTS tweets;
+-- DROP SEQUENCE IF EXISTS dagashis_id_seq;
 -- DROP TABLE IF EXISTS dagashis;
 -- DROP TABLE IF EXISTS dagashi_association;
---
+++ migrate/V9999__master_data.sql
@@ -0,0 +1,3 @@
+insert into users(id, name, image_url)VALUES
+  ('deltama37', 'delta', 'https://pbs.twimg.com/profile_images/1025324048132190208/RgImJmgx_400x400.jpg'),
+  ('osanpozuki', 'tama', 'https://pbs.twimg.com/profile_images/1082717517993336832/ZRguAkK1_400x400.jpg');
