73163c521e67ce38e2c0512a5bc937b58502a9af
Ryota Misumi <3delta7@gmail.com>
2019-03-24 14:40:35 +0900

    updagte

--- migrate/V9999__master_data.sql
+++ migrate/V9999__master_data.sql
@@ -7,7 +7,12 @@ insert into users(id, name, image_url)VALUES
 
 insert into tweets(tweet, like_count, retweet_count, image_url1, image_url2, image_url3, image_url4, user_id) VALUES
   ('クソワロタ', 0, 0, '', '', '', '', 'vaaaaanquish'),
-  ('「それ大体の技術でそうじゃん」様々な会話を終了に導ける力がある', 25, 0, '', '', '', '', 'vaaaaanquish') ;
+  ('「それ大体の技術でそうじゃん」様々な会話を終了に導ける力がある', 12, 2, '', '', '', '', 'vaaaaanquish'),
+  ('誰か滝行行こうや精神叩き直さな', 3, 0, '', '', '', '', 'MATSUURA_SHT'),
+  ('バチャコン一応参加したからあとでブログ書くか(なんで山梨大と横国なのにry)', 1, 0, '', '', '', '', 'm_nishiba'),
+  ('今日はお姉様方とお会いしてバレンタインのお返しをします', 2, 0, '', '', '', '', 'MATSUURA_SHT'),
+  ('興味深いツイート多いのでいいね連打した', 4, 1, '', '', '', '', 'deltama37'),
+  ('大学からの理工系人材の供給が追いついていないという話あるけど，日経古典企業だと大学院でCUDAプログラミングめちゃくちゃやってた人をなぜかコンサルに配置する事例があったりして，人材供給増やさくてもいいのではという気持ちになる', 2, 0, '', '', '', '', 'm_nishiba')  ;
 
 insert into dagashis(name, price, image_url)VALUES
   ('うまい棒', 10, ''),
